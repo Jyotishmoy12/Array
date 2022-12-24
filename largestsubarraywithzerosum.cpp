7f1840bae4fefca90b272161767956afb5f6dd39
Jyotishmoy Deka <93263133+Jyotishmoy12@users.noreply.github.com>
2022-12-24 19:19:01 +0530

    Update largestsubarraywithzerosum.cpp

--- largestsubarraywithzerosum.cpp
+++ largestsubarraywithzerosum.cpp
@@ -2,23 +2,20 @@ class Solution{
     public:
     int maxLen(vector<int>&A, int n)
     {   
-       unordered_map<int,int>mp;
-       int maxi=0; //stores the length of maximum subarray
-       int sum=0;// prefix sum
-       for(int i=0;i<n;i++){
+        // Your code here
+        unordered_map<int,int>mp;
+        int sum=0;
+        int ans=0;
+        mp[0]=-1;
+        for(int i=0;i<n;i++){
            sum+=A[i];
-           if(sum==0){
-               maxi=i+1;
+           if(mp.find(sum)!=mp.end()){
+               ans= max(ans,i-mp[sum]);
            }
            else{
-               if(mp.find(sum)!=mp.end()){
-                   maxi=max(maxi, i-mp[sum]);
-               }
-               else{
-                   mp[sum]=i;
-               }
+               mp[sum]=i;
            }
-       }
-       return maxi;
+        }
+        return ans;
     }
 };
